60867d612c29371ced2100fd5e911e2e9a4416c1
Andreas Walter <andreas.walter@waltronix.de>
2021-05-22 01:38:42 +0200

    build: clone and build open62541 with patches

--- build.rs
+++ build.rs
@@ -1,12 +1,17 @@
 use cmake::Config;
-use std::env;
 use std::path::PathBuf;
+use std::{env, path::Path};
+
+const OPEN6251_REPOSITORY_URL: &str = "https://github.com/open62541/open62541.git";
+const OPEN6251_REPOSITORY_PATH: &str = "open62541_src";
+const OPEN6251_TAG: &str = "v1.2.2";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    get_open62541();
     build_open62541();
-    generate_module("client");
+    // generate_module("client");
     generate_module("server");
 }
 
@@ -14,14 +19,48 @@ fn out_path() -> PathBuf {
     PathBuf::from(env::var("OUT_DIR").unwrap())
 }
 
+fn get_open62541() {
+    println!("get_open62541");
+
+    let out = std::process::Command::new("git")
+        .args(&["clone", OPEN6251_REPOSITORY_URL, OPEN6251_REPOSITORY_PATH])
+        .output()
+        .expect("could not clone repsitory");
+    println!("out: {:?}", out);
+
+    std::process::Command::new("git")
+        .args(&["checkout", OPEN6251_TAG])
+        .current_dir(OPEN6251_REPOSITORY_PATH)
+        .output()
+        .expect("could not check out tag");
+
+    let patch_dir = std::path::Path::new("patches");
+    println!("patch_dir: {:?}", patch_dir);
+    for entry in std::fs::read_dir(patch_dir).unwrap() {
+        let patch = entry.unwrap().path();
+        println!("patch: {:?}", patch);
+        let patch_file = Path::new("..").join(patch);
+        let patch_file = patch_file.as_os_str().to_str().unwrap();
+        std::process::Command::new("git")
+            .args(&["apply", patch_file])
+            .current_dir(OPEN6251_REPOSITORY_PATH)
+            .output()
+            .expect("could not apply patches");
+    }
+}
+
 fn build_open62541() {
-    let open62541_path = Config::new("open62541").generator("Ninja").build();
+    let open62541_path = Config::new(OPEN6251_REPOSITORY_PATH)
+        .generator("Ninja")
+        .very_verbose(true)
+        .build();
 
     println!(
         "cargo:rustc-link-search=native={}/lib",
         open62541_path.display()
     );
     println!("cargo:rustc-link-lib=static=open62541");
+    //sf
 }
 
 fn generate_module(module: &str) {
@@ -29,6 +68,7 @@ fn generate_module(module: &str) {
     let bindings = bindgen::Builder::default()
         .header(format!("{}.h", module))
         .clang_arg(format!("-I{}/include", open62541_path.display()))
+        .generate_inline_functions(true)
         .allowlist_function("UA_.*")
         .allowlist_type("UA_.*")
         .allowlist_var("UA_.*")
--- server.h
+++ server.h
@@ -1,2 +1,9 @@
+#include <open62541/architecture_definitions.h>
+
+#ifdef UA_STATIC_INLINE
+#   undef UA_STATIC_INLINE
+#   define UA_STATIC_INLINE extern inline
+#endif
+
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
